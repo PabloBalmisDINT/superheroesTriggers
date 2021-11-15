3283ca0dacec22176f7edc481195b296d0234492
Pablo <pablo.martinezaznar@iesdoctorbalmis.com>
2021-11-15 13:43:31 +0100

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace superheroes
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainWindowVM vm = new MainWindowVM();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = vm;
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) //click en las flechas para cambiar entre superheroes
+        {
+            vm.Avanzar();
+        }
+
+        private void Image_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            vm.Retroceder();
+        }
+    }
+}
---
+++ MainWindowVM.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace superheroes
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        //Campos con getters y setters
+        readonly private List<Superheroe> lista = Superheroe.GetSamples();
+        
+        private Superheroe superheroeActual;
+        public Superheroe SuperheroeActual 
+        { 
+            get { return superheroeActual; }
+            set 
+            { 
+                superheroeActual = value;
+                NotifyPropertyChanged("SuperheroeActual");
+            }
+        }
+        private int contadorActual;
+        public int ContadorActual 
+        {
+            get 
+            { 
+                return contadorActual; 
+            }
+            set
+            {
+                contadorActual = value;
+                NotifyPropertyChanged("ContadorActual");
+            } 
+        }
+        private int totalHeroes;
+        public int TotalHeroes 
+        { 
+            get
+            {
+                return totalHeroes;
+            }
+            set
+            {
+                totalHeroes = value;
+                NotifyPropertyChanged("TotalHeroes");
+            }
+        }
+
+        //Constructor
+        public MainWindowVM()
+        {
+            SuperheroeActual = lista.FirstOrDefault<Superheroe>();
+            ContadorActual = 1;
+            TotalHeroes = lista.Count;
+        }
+
+        //Metodos
+        public void Avanzar()
+        {
+            if (ContadorActual < TotalHeroes)
+            {
+                ContadorActual++;
+                SuperheroeActual = lista[ContadorActual - 1];
+            }
+        }
+
+        public void Retroceder()
+        {
+            if(ContadorActual > 1)
+            {
+                ContadorActual--;
+                SuperheroeActual = lista[ContadorActual - 1];
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
